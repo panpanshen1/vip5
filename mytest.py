d1409af2740d46ba88c787710ec415de2e747cc2
panpan <1106225179@qq.com>
2020-02-29 18:50:43 +0800

    第一节课程

---
+++ mytest.py
@@ -0,0 +1,52 @@
+a=2
+# b=100
+# c=2000
+# print(('%d,%f,%s')%(a,b,c))
+# print('a={},b={},c={}'.format(a,b,c))
+
+
+#
+# a = (1,2,3,4,5,6,7,8,9)
+# print(a[-3])
+# print(a[3:8:2])
+#
+#
+#
+# print(tuple(list(range(1,1000))))
+
+
+# a=[1,2,3,4,5,6,7,8,9]
+# a.reverse()
+# print(a)
+
+# a=[3,5,8,5,4,2,9,1,2]
+# b=[543,6]
+# # a.sort()
+# # print(a)
+# a.insert(2,9)
+# print(a)
+# a.append(8)
+# a.extend(b)
+# a.pop(6)
+# a.remove(5)
+# del a[3]
+# print(a)
+# print(sorted(a))
+# print(a.index(6))
+# print(len(a))
+#
+# print(min(a))
+# print(max(a))
+# print(a.count(2))
+# print(a)
+# print(1 in a)
+# print(555 not in a)
+# print(a.clear())
+
+
+#a=2
+a+=2+3
+a*=3+3
+#先算等号右边，然后算乘号
+a*=6
+print(a)
