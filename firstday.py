b25f85cffa0bf87af7e5a35cd8ecba3af9dee554
panpan <1106225179@qq.com>
2020-03-04 21:52:20 +0800

    第一天作业

---
+++ firstday.py
@@ -0,0 +1,15 @@
+a = [11,13,5,7,0,56,23,34,72]
+
+print(min(a),max(a),len(a))
+print(a.index(56))
+a.append(7)
+print(a)
+a.pop(4)
+print(a)
+a.sort()
+print(a)
+
+
+b=[66,67,68]
+a.extend(b)
+print(a)
