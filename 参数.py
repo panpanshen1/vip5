dafc8a04b0f712011dc0b8f7f96828a4acd354b4
panpan <1106225179@qq.com>
2020-03-07 16:46:34 +0800

    第二天

---
+++ 参数.py
@@ -0,0 +1,28 @@
+#默认参数
+# def add_end(L=None):
+#     if L is None:
+#         L=[]
+#         L.append('end')
+#         return L()
+#返回给print
+# print(add_end())
+# print(add_end())
+
+
+#可变参数,只能是元组/列表，*args
+# def calc(*numbers):
+#     print(*numbers)
+#     print(numbers)
+#     sum=0
+#     for n in numbers:
+#         sum += n
+#     return sum
+# print(calc(1,6,4,3,2))
+
+
+#关键字参数，**kwargs
+
+
+
+
+
