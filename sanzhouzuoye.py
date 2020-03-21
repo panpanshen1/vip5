932176c94094c64880cd029416402695732bb2f2
panpan <1106225179@qq.com>
2020-03-21 11:11:18 +0800

    类

--- sanzhouzuoye.py
+++ sanzhouzuoye.py
@@ -167,3 +167,69 @@
 #属性：名字
 #方法：发射，装填
 
+
+
+# 3、摆放家具
+# 需求：
+# 1）.房子有户型，总面积和家具名称列表
+#    新房子没有任何的家具
+# 2）.家具有名字和占地面积，其中
+#    床：占4平米
+#    衣柜：占2平面
+#    餐桌：占1.5平米
+# 3）.将以上三件家具添加到房子中
+# 4）.打印房子时，要求输出:户型，总面积，剩余面积，家具名称列表
+
+'''
+#房子类 House
+属性：户型apartment  总面积 total_area  家具列表furniture_list  剩余面积 residual_area
+方法：添加家具add_furniture
+
+
+#家具类Furniture
+属性：名字name 占地面积cover_area
+'''
+class House(object):
+    def __init__(self, apartment, total_area):
+        self.furniture_list = []
+        self.apartment = apartment
+        self.total_area = total_area
+        self.residual_area = self.total_area
+    def add_furniture(self, item):
+        # 家具名称列表新增家具名称
+        print('开始添加家具')
+        self.furniture_list.append(item.name)
+        # 剩余面积会减少（剩余面积 = 总面积-家具占地面积）
+        self.residual_area  = self.residual_area -item.cover_area
+        print('添加家具成功')
+        pass
+    # 返回实例对象的类的描述信息，
+    def __str__(self):
+        return '创建了一个户型为%s,面积为%s平的房子,剩余面积为%s平,家具有%s的房子' % (self.total_area, self.apartment,self.residual_area,self.furniture_list)
+    #py自己的东西，__str__,用return，调用类的时候就会打印出来了
+class Furniture(object):
+    def __init__(self,name,cover_area):
+        self.name = name
+        self.cover_area = cover_area
+    def __str__(self):
+        return '创建了一个%s家具，占地面积为%s'%(self.name,self.cover_area)
+
+
+h = House('三室一厅',120)
+print(h)
+
+f =Furniture('床',4)
+
+h.add_furniture(f)
+print(h)
+
+
+# class strtest:
+#     def __init__(self):
+#         print("init: this is only test")
+#     def __str__(self):
+#         return "str: this is only test"
+#
+# if __name__ == "__main__":
+#     st=strtest()
+#     print (st)
