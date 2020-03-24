4dd99d964d2363199f20a1aa6c143d3593325664
panpan <1106225179@qq.com>
2020-03-24 22:35:46 +0800

    课程和作业

--- os和string.py
+++ os和string.py
@@ -1,8 +1,48 @@
-import os
+import os,stat
 #os.chdir(path)
 #改变当前工作目录
 #os.getcwd()
-def print_path():
-    print_path(os.getcwd())
+# def print_path():
+#     print_path(os.getcwd())
 
 import requests
+'''
+print(os.access('/Users/shenpanpanpan/Desktop/未命名文件夹/第二天/1.txt',os.R_OK))
+os.F_OK: 作为access()的mode参数，测试path是否存在。
+os.R_OK: 包含在access()的mode参数中 ， 测试path是否可读。
+os.W_OK 包含在access()的mode参数中 ， 测试path是否可写。
+os.X_OK 包含在access()的mode参数中 ，测试path是否可执行。
+
+path = '/Users/shenpanpanpan/Desktop/未命名文件夹/第二天/1.txt'
+print(os.getcwd())
+os.chdir('/Users/shenpanpanpan/Desktop/code/interfaceTestVip5/')
+修改目录到
+print(os.getcwd())
+查看修改后的路径
+os.chdir('/Users/shenpanpanpan/Desktop/code/vip5')
+print(os.getcwd())
+#os.chdir('/Users/shenpanpanpan/Desktop/code/interface')
+
+path = '/Users/shenpanpanpan/Desktop/未命名文件夹/第二天/1.txt'
+os.chmod( '/Users/shenpanpanpan/Desktop/未命名文件夹/第二天/1.txt', stat.S_IXGRP)
+os.chown('/Users/shenpanpanpan/Desktop/未命名文件夹/第二天/1.txt',100,-1)
+
+os.chroot('/User/)
+改变当前的根目录
+
+os.close(fd)
+关闭目录
+
+print(os.path.abspath('第二天/1.txt'))
+返回绝对路径
+print(os.path.basename('/Users/shenpanpanpan/Desktop/未命名文件夹/第二天/1.txt'))
+返回文件名
+print(os.path.dirname('第二天/1.txt'))
+返回文件路径，相对路径
+
+print(os.path.exists('第二天/1.txt'))
+路径存在则返回True，路径损坏则返回False
+
+print(os.path.dirname(__file__))
+
+'''
--- sanzhouzuoye.py
+++ sanzhouzuoye.py
@@ -251,25 +251,90 @@
 方法：装填 put  发射 Lunch
 '''
 
+# class Gun(object):
+#     def __init__(self,gunname):
+#         self.gunname = gunname
+#     # def  put(self):
+#     #     print('%s可以装填子弹'%(self.gunname))
+#     # def  lunch(self):
+#     #     print('%s可以发射子弹' % (self.gunname))
+#     def __str__(self):
+#         return '%s可以装填子弹,%s可以发射子弹'%(self.gunname,self.gunname)
+# class Soldier(object):
+#     def __init__(self,name):
+#         self.name = name
+#     def fire(self):
+#         print('士兵可以开火')
+#     def __str__(self):
+#         return '%s可以装填子弹,%s可以发射子弹' % (self.gunname, self.gunname)
+#
+# g = Gun('ak47')
+# print(g)
+# s=Soldier('sd')
+# print(s)
+
+#coding:utf-8
+
+#
+# 4.士兵开枪
+# 需求：
+# 1）.士兵瑞恩有一把AK47
+# 2）.士兵可以开火(士兵开火扣动的是扳机)
+# 3）.枪 能够 发射子弹(把子弹发射出去)
+# 4）.枪 能够 装填子弹 --增加子弹的数量
+
+'''
+'''
+
+
+
+
+
 class Gun(object):
-    def __init__(self,gunname):
-        self.gunname = gunname
-    # def  put(self):
-    #     print('%s可以装填子弹'%(self.gunname))
-    # def  lunch(self):
-    #     print('%s可以发射子弹' % (self.gunname))
-    def __str__(self):
-        return '%s可以装填子弹,%s可以发射子弹'%(self.gunname,self.gunname)
-class Soldier(object):
+
     def __init__(self,name):
         self.name = name
-    def fire(self):
-        print('士兵可以开火')
+        self.bullet_count = 0
+
     def __str__(self):
-        return '%s可以装填子弹,%s可以发射子弹' % (self.gunname, self.gunname)
+        return "制造一把%s有%d发子弹" % (self.name,self.bullet_count)
 
-g = Gun('ak47')
-print(g)
-s=Soldier('sd')
-print(s)
+    def add_bullet(self):
+        self.bullet_count += 20
+        print('%d发子弹装填完毕'%self.bullet_count)
 
+    def shot(self):
+        self.bullet_count -= 1
+        print('发射子弹1发，还剩%d发' % self.bullet_count)
+
+
+class Soilder(object):
+
+    def __init__(self,name):
+        self.name = name
+        # self.bullet = 0
+
+    def __str__(self):
+        return "士兵%s" % self.name
+
+    def fire(self,item):
+
+        if item.bullet_count > 0 :
+            # print('%s子弹充足，进行射击' % item.name)
+            print('士兵射击')
+            item.shot()
+        else:
+            print('%s子弹不足，需要装填子弹' % item.name)
+            item.add_bullet()
+
+
+
+AK = Gun("AK47")
+print(AK)
+
+s1 = Soilder('瑞恩')
+print(s1)
+s1.fire(AK)
+s1.fire(AK)
+s1.fire(AK)
+s1.fire(AK)
